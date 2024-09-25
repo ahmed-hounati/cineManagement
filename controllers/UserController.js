561db2f8a0cf3afc7f956ea2e787d7fda6766068
ahmed-hounati <hounatiahmed@gmail.com>
2024-09-25 12:17:44 +0100

    Feat: Add a function that return the user info Description : Implemented the me function that return the user info's

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -203,6 +203,27 @@ class UserController {
     }
 
 
+    async me(req, res) {
+        try {
+            const token = req.headers['authorization']?.split(' ')[1];
+            if (!token) {
+                return res.status(401).json({ message: 'No token provided' });
+            }
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const userIDe = decoded.id;
+            const user = await UserDAO.findById(userIDe);
+
+            if (!user) {
+                return res.status(401).json({ message: 'try again later ...' });
+            }
+            res.status(200).json(user);
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid or expired token' });
+        }
+    }
+
+
 }
 
 module.exports = new UserController();
